118f00f7813e31f9e33f9ac1bc7119f96bbe58ed
Anna Beddow <anna.beddow@gmail.com>
2017-04-07 10:45:38 +0100

    added host and serverURL for fetching data from server side

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 import config from './config';
 import apiRouter from './api';
+import path from 'path';
 
 import express from 'express';
 const server = express();
 
 server.set('view engine', 'ejs');
 
+import './serverRender';
+
 server.get('/', (req, res) => {
   res.render('index', {
-    content: 'Url Shortener'
+    content: '...'
   });
 });
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
-server.listen(config.port, () => {
+server.listen(config.port, config.host, () => {
   console.info('Express listening on port ', config.port);
 });
