543b21d437eb07ebdf119cf006ccf717d3d05911
Anna Beddow <anna.beddow@gmail.com>
2017-04-07 09:45:10 +0100

    made app a class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div className="App">
-    <Header message = "Naming Contests" />
-    <div>
-    ...
-    </div>
-    </div>
 
-  );
-};
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+      <Header message = "Naming Contests" />
+      <div>
+      ...
+      </div>
+      </div>
+
+    );
+  }
+}
+
 
 export default App;
