ea6f16c1a1c79b56727172b4e9c2820cab1a8d9f
Anna Beddow <anna.beddow@gmail.com>
2017-04-07 11:02:53 +0100

    added mongoose schema and db get for all urls

--- api/index.js
+++ api/index.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+var Schema = mongoose.Schema;
+
+var urlSchema = new Schema ({
+  longUrl: {type: String, required: true},
+  shortId: {type: Number, required: true}
+});
+
+var UrlData = mongoose.model('UrlData', urlSchema);
 
 const router = express.Router();
 
 router.get('/urls', (req, res) => {
-  res.send({ urls: [] });
+  UrlData.find()
+  .then(function(doc) {
+    res.send(doc);
+  });
 });
 
 export default router;
