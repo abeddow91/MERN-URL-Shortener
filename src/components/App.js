1bdbce7efeb729f3f051986838bf89b93ab3ffa3
Anna Beddow <anna.beddow@gmail.com>
2017-04-07 10:20:19 +0100

    added state to app

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,15 @@ import Header from './Header';
 
 
 class App extends Component {
+
+  state = {
+    pageHeader: 'Url Shortener'
+  };
+
   render () {
     return (
       <div className="App">
-      <Header message = "URL Shortener" />
+      <Header message = {this.state.pageHeader} />
       <div>
       </div>
       </div>
